bcd32a4b87ae71b7cb0f6f910fcb36daad6d44ae
Hasan Fırat Keskin <hfk4747@gmail.com>
2023-06-18 00:51:02 +0300

    ilk yükleme

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace bayram_ali_dursun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel5});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 573);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1045, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "OgrNo_BayramAliDursun";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(201, 20);
+            this.toolStripStatusLabel1.Text = "213302116_BayramAliDursun";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(242, 20);
+            this.toolStripStatusLabel3.Spring = true;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 18);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(242, 20);
+            this.toolStripStatusLabel4.Spring = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(540, 540);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(561, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(472, 150);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ayarlar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Oyuna başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oyuncu Adı Giriniz:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 93);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 26);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Zor";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 61);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 26);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Orta";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 26);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kolay";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(561, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(472, 357);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Puan Tablosu";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(465, 321);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(614, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mayın Sayısı = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(716, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(943, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(896, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Puan:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(40, 20);
+            this.toolStripStatusLabel2.Text = "Tarih";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(13, 20);
+            this.toolStripStatusLabel5.Text = " ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 599);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "213302116_BayramAliDursun";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace bayram_ali_dursun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Application.StartupPath + "\\Puanlar.mdb");
+        OleDbCommand command = new OleDbCommand();
+
+        public static int MayinSayisi;
+        public static int MatrisKenarSayisi = 10;
+        public static int ToplamKareSayisi = MatrisKenarSayisi * MatrisKenarSayisi;
+        public static int Puan;
+
+        int[] MayinKonumlari;
+        int PuanCarpani;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VeriTabaniniGoster();
+        }
+
+        public void VeriTabaniniGoster()
+        {
+            string SQLkomutu = "select *from puan_tablosu";
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            command.CommandText = SQLkomutu;
+            command.Connection = connection;
+
+            OleDbDataAdapter adaptor = new OleDbDataAdapter(command);
+            DataTable datatablosu = new DataTable();
+            adaptor.Fill(datatablosu);
+            dataGridView1.DataSource = datatablosu;
+            connection.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SiraIndexi = -1;
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString().Equals(textBox1.Text))
+                {
+                    SiraIndexi = i;
+                    break;
+                }
+            }
+
+            if (textBox1.Text == "")
+                MessageBox.Show("Lütfen Oyuncu Adı Ekleyiniz!");
+            else if (SiraIndexi != -1)
+                MessageBox.Show("Daha önceden kayıt edilmiş bir isim girdiniz. Lütfen farklı bir isim girin!");
+            else
+            {
+                if (radioButton1.Checked) { MayinSayisi = 10; PuanCarpani = 1; }
+                else if (radioButton2.Checked) { MayinSayisi = 25; PuanCarpani = 3; }
+                else if (radioButton3.Checked) { MayinSayisi = 40; PuanCarpani = 5; }
+
+                label3.Text = "Mayın Sayısı = " + MayinSayisi.ToString();
+                toolStripProgressBar1.Maximum = ToplamKareSayisi - MayinSayisi; // processbar'ın maksimum değeri güncelleniyor
+                toolStripProgressBar1.Value = 0;
+
+                flowLayoutPanel1.Controls.Clear();
+                MayinKonumlari = new int[MayinSayisi];
+                Puan = 0;
+                label3.Text = "0";
+                Random rnd = new Random();
+
+                for (int sayace = 0; sayace < MayinSayisi; sayace++)
+                {
+                    int sayi = rnd.Next(1, ToplamKareSayisi);
+                    if (MayinKonumlari.Contains(sayi))
+                    {
+                        continue;
+                    }
+                        
+                    MayinKonumlari[sayace] = sayi;
+                }
+
+                int sayac = 1;
+                for (int i = 1; i <= MatrisKenarSayisi; i++)
+                {
+                    for (int j = 1; j <= MatrisKenarSayisi; j++)
+                    {
+                        Button mayinbuton = new Button() { Name = "buton" + string.Format(sayac.ToString()), Tag = sayac.ToString() };
+                        mayinbuton.Size = new Size(40, 40);
+                        mayinbuton.Margin = new Padding(0, 0, 0, 0);
+                        mayinbuton.Click += new EventHandler(butona_bas);
+                        mayinbuton.BackColor = Color.Blue;
+                        sayac++;
+                        flowLayoutPanel1.Controls.Add(mayinbuton);
+                    }
+                }
+            }
+        }
+
+        private void butona_bas(object sender, EventArgs e)
+        {
+            Button mayinbutonu = sender as Button;
+
+            if (MayinKonumlari.Contains(Convert.ToInt32(mayinbutonu.Tag)))
+            {
+                mayinbutonu.BackColor = Color.Red;
+                MayinlariGoster();
+                MessageBox.Show("Yandınız\nToplam Puan:" + Puan);
+                VeritabaninaKaydet();
+            }
+            else
+            {
+                mayinbutonu.BackColor = Color.Green;
+                Puan = Puan + PuanCarpani;
+                mayinbutonu.Text = Mayintarama(Convert.ToInt32(mayinbutonu.Tag)).ToString();
+                toolStripProgressBar1.Value++;
+                label4.Text = Puan.ToString();
+                mayinbutonu.Enabled = false;
+            }
+        }
+
+        private int Mayintarama(int v)
+        {
+            int myn = 0;
+            if (v <= MatrisKenarSayisi) //tıklanan buton ilk satırda mı
+            {
+                if (MayinKonumlari.Contains(v - 1)) myn++;
+                if (MayinKonumlari.Contains(v + 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi - 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi + 1)) myn++;
+            }
+            else if (v % 10 == 1)  //tıklanan buton ilk sütunda mı
+            {
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi + 1)) myn++;
+                if (MayinKonumlari.Contains(v + 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi + 1)) myn++;
+            }
+            else if (v % 10 == 0) //tıklanan buton son sütunda  mı
+            {
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi - 1)) myn++;
+                if (MayinKonumlari.Contains(v - 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi - 1)) myn++;
+            }
+            else  //diğer durumlar
+            {
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi - 1)) myn++;
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v - MatrisKenarSayisi + 1)) myn++;
+                if (MayinKonumlari.Contains(v - 1)) myn++;
+                if (MayinKonumlari.Contains(v + 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi - 1)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi)) myn++;
+                if (MayinKonumlari.Contains(v + MatrisKenarSayisi + 1)) myn++;
+
+            }
+            return myn;
+        }
+
+        private void VeritabaninaKaydet()
+        {
+            string SQLkomutu = "INSERT INTO puan_tablosu(Ad,Puan,Tarih) VALUES('" + textBox1.Text + "'," + Puan + ",'" + DateTime.Now.Date + "');";
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            command.CommandText = SQLkomutu;
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+            connection.Close();
+            VeriTabaniniGoster();
+        }
+
+        private void MayinlariGoster()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (MayinKonumlari.Contains(i))
+                {
+                    flowLayoutPanel1.Controls[i - 1].BackColor = Color.Red;
+                    flowLayoutPanel1.Controls[i - 1].BackgroundImage = Image.FromFile("mayin.png");
+                }
+                else
+                {
+                    flowLayoutPanel1.Controls[i - 1].BackColor = Color.Green;
+                    flowLayoutPanel1.Controls[i - 1].Text = Mayintarama(i).ToString();
+                }
+                flowLayoutPanel1.Controls[i - 1].Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = DateTime.Now.ToString();
+        }
+    }
+}
